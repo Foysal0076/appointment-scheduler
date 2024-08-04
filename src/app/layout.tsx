134047bd2b597dc2738d50603da456a042e3548b
Foysal Ahmed <foysalxahmed@gmail.com>
2024-08-04 12:34:21 +0600

    Initiate project

---
+++ src/app/layout.tsx
@@ -0,0 +1,59 @@
+import '@/styles/globals.css'
+import '@/styles/globals.scss'
+
+import { Inter, Rubik } from 'next/font/google'
+
+import { AuthProvider } from '@/auth/AuthProvider'
+import NextThemeProvider from '@/components/Common/NextThemeProvider'
+import { Footer } from '@/components/Footer'
+import { Navbar } from '@/components/Navbar'
+import { PROJECT_INFORMATION } from '@/utils/constants'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+const rubik = Rubik({
+  subsets: ['latin'],
+  variable: '--font-rubik',
+  display: 'swap',
+})
+
+export const metadata = {
+  title: PROJECT_INFORMATION.name,
+  description: PROJECT_INFORMATION.description,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang='en'
+      suppressHydrationWarning
+      className={`${inter.variable} ${rubik.variable}`}>
+      <link
+        rel='icon'
+        type='image/ico'
+        sizes='32x32'
+        href='/assets/favicons/favicon.ico'
+      />
+      <body>
+        <AuthProvider>
+          <NextThemeProvider>
+            <div className='flex min-h-screen flex-col justify-between bg-surface-50 pt-[3.75rem] md:pt-[4.5rem]'>
+              <div>
+                <Navbar />
+                <main>{children}</main>
+              </div>
+              <Footer />
+            </div>
+          </NextThemeProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/utils/constants/projectInformation.ts
@@ -0,0 +1,6 @@
+import { ProjectInformation } from '@/utils/types'
+
+export const PROJECT_INFORMATION: ProjectInformation = {
+  name: 'Appointment Scheduler',
+  description: 'A simple appointment scheduler',
+}
