956c21e496e71be25ee476e5394ad2b168378954
Foysal Ahmed <foysalxahmed@gmail.com>
2024-08-07 06:43:09 +0600

    Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+
+import { authOptions } from '@/auth/options'
 import { HomeHero } from '@/components/Home'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const session = await getServerSession(authOptions)
+  // console.log(session)
+  if (session && session.accessToken) {
+    return redirect('/dashboard')
+  }
+
   return (
     <div>
       <HomeHero />
--- src/components/Navbar/NavigationDrawer/DrawerContent.tsx
+++ src/components/Navbar/NavigationDrawer/DrawerContent.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 export const DrawerContent = () => {
   const year = new Date().getFullYear()
   return (
@@ -9,24 +7,13 @@ export const DrawerContent = () => {
       <div className='flex grow flex-col justify-between gap-4'>
         <div>{/* Top navigation */}</div>
         <div className='flex flex-col justify-end gap-4 bg-surface-50 px-6 py-4 dark:bg-surface-100/20'>
-          <Link
-            href='/about'
-            className='text-sm transition-colors duration-300 hover:text-primary-500'>
-            About
-          </Link>
-          <Link
-            href='/contact'
-            className='mb-4 text-sm transition-colors duration-300 hover:text-primary-500'>
-            Contact
-          </Link>
-          <p className={`flex items-center text-xs`}>
-            © {year}. All rights reserved.
-          </p>
+          <div className='flex items-center justify-between'>
+            <p className={`flex items-center text-xs`}>
+              © {year}. All rights reserved.
+            </p>
+            <p className='text-xs italic'>Foysal</p>
+          </div>
         </div>
-      </div>
-      <div className='flex gap-8 px-6 py-4 shadow-top'>
-        {/* Other content */}
-        Other Content
       </div>
     </nav>
   )
